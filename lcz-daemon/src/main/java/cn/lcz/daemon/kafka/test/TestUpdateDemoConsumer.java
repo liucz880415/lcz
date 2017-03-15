1d9ad98d5b9f6eae88cf7b6711a0c413f5356b0c
liuchuanzhu <531393658@qq.com>
2017-03-15 09:35:28 +0800

    change spring config files

--- lcz-daemon/src/main/java/cn/lcz/daemon/kafka/test/TestUpdateDemoConsumer.java
+++ lcz-daemon/src/main/java/cn/lcz/daemon/kafka/test/TestUpdateDemoConsumer.java
@@ -28,33 +28,28 @@ public class TestUpdateDemoConsumer extends AbstractUpdateConsumer {
         Map<UpdateTopic, List<KafkaUpdateListener>> listeners = Maps.newHashMap();
 
         // 添加对于每一个Topic的订阅处理
-        listeners.put(UpdateTopic.TEST_UPDATE_KAFKA, getStaffRosterListeners());
+        listeners.put(UpdateTopic.TEST_UPDATE_KAFKA, Lists.newArrayList(
+                new KafkaUpdateListener(KafkaTag.NEW) {
+                    @Override
+                    protected void onUpdate(String companyId, List oldData, List newData) {
+                        System.out.println("new add entity handle...");
+                    }
+                },
+                new KafkaUpdateListener<KafkaUpdateEntity>(KafkaTag.UPDATE) {
+                    @Override
+                    protected void onUpdate(String companyId, List<KafkaUpdateEntity> oldData, List<KafkaUpdateEntity> newData) {
+                        System.out.println("update entity handle...");
+                    }
+                },
+                new KafkaUpdateListener<KafkaUpdateEntity>(KafkaTag.DELETE) {
+                    @Override
+                    protected void onUpdate(String companyId, List<KafkaUpdateEntity> oldData, List<KafkaUpdateEntity> newData) {
+                        System.out.println("delete entity handle...");
+                    }
+                }
+        ));
 
         return listeners;
     }
 
-    private List<KafkaUpdateListener> getStaffRosterListeners() {
-        List<KafkaUpdateListener> listeners = Lists.newArrayList();
-
-        listeners.add(new KafkaUpdateListener(KafkaTag.NEW) {
-            @Override
-            protected void onUpdate(String companyId, List oldData, List newData) {
-                System.out.println("new add entity handle...");
-            }
-        });
-
-        listeners.add(new KafkaUpdateListener<KafkaUpdateEntity>(KafkaTag.UPDATE) {
-            @Override
-            protected void onUpdate(String companyId, List<KafkaUpdateEntity> oldData, List<KafkaUpdateEntity> newData) {
-                System.out.println("update entity handle...");
-            }
-        });
-        listeners.add(new KafkaUpdateListener<KafkaUpdateEntity>(KafkaTag.DELETE) {
-            @Override
-            protected void onUpdate(String companyId, List<KafkaUpdateEntity> oldData, List<KafkaUpdateEntity> newData) {
-                System.out.println("delete entity handle...");
-            }
-        });
-        return listeners;
-    }
 }
